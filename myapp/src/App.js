88f6530da80b8b7656c3cccaf2f3097162c28db6
lipojack <cp000123@gmail.com>
2017-11-12 04:52:59 +0800

    load more information

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 //import list from './list';
-import { Grid, Row } from 'react-bootstrap';
+import { Grid, Row, FormGroup } from 'react-bootstrap';
 import './App.css';
 
 const DEFAULT_QUERY = 'react';
+const DEFAULT_PAGE = 0;
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-//const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+const PARAM_PAGE = 'page='
 
-function isSearched(searchTerm){
-  return function(item){
-    return !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
-  }
-}
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}${DEFAULT_PAGE}`;
+console.log(url);
+
+// function isSearched(searchTerm){
+//   return function(item){
+//     return !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+//   }
+// }
 
 class App extends Component {
   
@@ -29,21 +33,30 @@ class App extends Component {
     this.searchValue = this.searchValue.bind(this);
     this.setTopStories = this.setTopStories.bind(this);
     this.fetchTopStories = this.fetchTopStories.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   setTopStories(result){
-    this.setState({ result: result });
+    const {hits, page} = result;
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [...oldHits, ...hits];
+    this.setState({ result: { hits: updatedHits, page } });
   }
 
-  fetchTopStories(searchTerm){
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`)
+  fetchTopStories(searchTerm, page){
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
       .then(response => response.json())
       .then(result => this.setTopStories(result))
       .catch(e => e);
   }
 
   componentDidMount(){
+    this.fetchTopStories(this.state.searchTerm, DEFAULT_PAGE);
+  }
+
+  onSubmit(event, DEFAULT_PAGE){
     this.fetchTopStories(this.state.searchTerm);
+    event.preventDefault();
   }
 
   // ES6 
@@ -62,41 +75,66 @@ class App extends Component {
   render() {
     // Destructing
     const {result, searchTerm} = this.state;
-    if (!result) return null;
+    const page = (result && result.page) || 0;
     return (
       <div className = "App">
         
-        <Grid>
+        <Grid fluid>
           <Row>
             <div className="jumbotron">
               <Search 
                 onChange = {this.searchValue} 
                 value = {searchTerm}
+                onSubmit = { this.onSubmit }
               >Search : </Search>
             </div>
           </Row>
         </Grid>
+        
+        { result &&
+          <Table
+            list = {result.hits}
+            searchTerm = {searchTerm}
+            removeItem = {this.removeItem}
+          />
+        }
+          
+          <div className = "text-center alert">
+            <Button
+              className = "btn btn-success"
+              onClick = { () => this.fetchTopStories(searchTerm, page + 1) }>
+              Load more
+            </Button>
+          </div>
+          
 
-        <Table
-          list = {result.hits}
-          searchTerm = {searchTerm}
-          removeItem = {this.removeItem}
-        />
       </div>
+
+      
+      
     );
   }
 }
 
 // Stateless functional Component
-const Search = ({onChange, value, children}) =>{
+const Search = ({onChange, value, children, onSubmit}) =>{
   return(
-    <form>
-      {children}
-      <input 
-        type="text" 
-        onChange = { onChange } 
-        value = {value} 
-      />
+    <form onSubmit = { onSubmit }>
+      <FormGroup>
+        <div className = 'input-group'>
+          <input
+            className = 'form-control width100'
+            type = 'text' 
+            onChange = { onChange } 
+            value = {value} 
+          />
+          <span className = 'input-group-btn'>
+            <Button className = 'btn btn-primary' type = 'submit'>
+              Search
+            </Button>
+          </span>
+        </div>
+      </FormGroup>
     </form>
   )
 }
@@ -107,11 +145,13 @@ const Table = ({list, searchTerm, removeItem}) =>{
     <div>
       {
         // Don't use index as key : https://medium.com/@robinpokorny/index-as-a-key-is-an-anti-pattern-e0349aece318
-        list.filter( isSearched(searchTerm) ).map((item)=>
+        // list.filter( isSearched(searchTerm) ).map((item)=>
+        list.map((item)=>
             <div key={item.title}>
               <h1><a href ={item.url}>{item.title}</a> by {item.author}</h1>
-              <h2>comment : {item.comment} | point : {item.point}</h2>
+              <h2>comment : {item.num_comments} | point : {item.points}</h2>
               <Button
+                className = 'btn btn-danger'
                 type = 'button'
                 onClick = {()=>removeItem(item.title)}>
                 Remove
@@ -124,9 +164,11 @@ const Table = ({list, searchTerm, removeItem}) =>{
 }
 
 // Stateless functional Component
-const Button = ({ onClick, children }) =>
+const Button = ({ onClick, children, className }) =>
   <button 
-    onClick = {onClick}>
+    onClick = { onClick }
+    className = { className }
+    >
     {children}
   </button>
 
